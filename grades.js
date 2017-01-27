1d85d55484a2a392c20103ec48bddbfc461ee79c
katherine tuter <katherinetuter@gmail.com>
2017-01-26 20:33:01 -0600

    finished all reqs--finding low and high grades

--- grades.js
+++ grades.js
@@ -1,17 +1,7 @@
-// Loop over an array of student grades (values from 50-100) and outputs how many of each grades there are.
-
-// A score of 50-60 is an F
-// A score of 61-70 is a D
-// A score of 71-80 is a C
-// A score of 81-90 is a B
-// A score of 91-100 is an A
-// Start with array of random scores in your JavaScript
-// Use console.log to output the following criteria:
-// How many of each grade?
-// What is the lowest grade?
-// What is the highest grade?
 
 var scores = [82, 71, 95, 55, 98, 69, 72, 78, 84, 64, 58, 87];
+
+//setting variables for different letter grades
 
 var a = 0
 var b = 0
@@ -19,14 +9,41 @@ var c = 0
 var d = 0
 var f = 0
 
-for (var n = 0; n<scores.length; n++) {
+//setting the loop to find number of scores in each range
+
+for (var n = 0; n<scores.length; n++){
 	if (scores[n] > 90) {
- 		a = a+1	
- 	} else if (scores[n] > 80) {
- 		
- 	}
+ 		a = a+1;	
+ 	} else if (scores[n] > 80){
+ 		b = b+1;
+ 	} else if (scores[n] > 70){
+ 		c = c+1;
+ 	} else if (scores[n] > 60){
+ 		d = d+1;
+ 	} else
+ 		f = f+1;	
  }
 
+//outputting letter grade totals to console
+
 console.log("Total of As: ", a);
+console.log("Total of Bs: ", b);
+console.log("Total of Cs: ", c);
+console.log("Total of Ds: ", d);
+console.log("Total of Fs: ", f);
+
+//what is the lowest grade?
+
+scores.sort();
+console.log("The lowest grade is: ", scores[0]);
+
+//what is the highest grade?
+
+console.log("The highest grade is: ", scores.slice(-1)[0]);
+
+
+
+
+
  
 
